a37a5f0aa06e3503c9bcfc0eb63d7d8c346d224a
Diana5355 <dianasosina005@mail.ru>
2023-01-31 20:56:05 +0200

    dataprovider

--- Qa36_Phonebook/src/main/java/manager/DataProviderUser.java
+++ Qa36_Phonebook/src/main/java/manager/DataProviderUser.java
@@ -10,40 +10,44 @@ import java.util.Iterator;
 import java.util.List;
 
 public class DataProviderUser {
-
     @DataProvider
     public Iterator<Object[]> loginDataCls(){
+
         List<Object[]> list = new ArrayList<>();
 
-        list.add(new Object[]{"noa@gmail.com", "Nnoa12345$"});
-        list.add(new Object[]{"sonya@gmail.com", "Ss12345$"});
-        list.add(new Object[]{"noa@gmail.com", "Nnoa12345$"});
+        list.add(new Object[]{"noa@gmail.com","Nnoa12345$"});
+        list.add(new Object[]{"sonya@gmail.com","Ss12345$"});
+        list.add(new Object[]{"noa@gmail.com","Nnoa12345$"});
 
         return list.iterator();
+
     }
+
 
     @DataProvider
     public Iterator<Object[]> loginDataUser(){
         List<Object[]> list = new ArrayList<>();
 
-        list.add(new Object[]{"noa@gmail.com", "Nnoa12345$"});
-        list.add(new Object[]{"sonya@gmail.com", "Ss12345$"});
-        list.add(new Object[]{"noa@gmail.com", "Nnoa12345$"});
+        list.add(new Object[]{User.builder().email("noa@gmail.com").password("Nnoa12345$").build()});
+        list.add(new Object[]{User.builder().email("sonya@gmail.com").password("Ss12345$").build()});
+        list.add(new Object[]{User.builder().email("noa@gmail.com").password("Nnoa12345$").build()});
 
         return list.iterator();
-    }
 
+    }
     @DataProvider
     public Iterator<Object[]> loginDataUserFromFile() throws IOException {
         List<Object[]> list = new ArrayList<>();
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("Qa36_Phonebook/src/test/resources/data.csv")));
-        String line = bufferedReader.readLine(); // noa@gmail.com,Nnoa12345$
-        while(line != null){
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("src/test/resources/data.csv")));
+        String  line =bufferedReader.readLine();  // "noa@gmail.com,Nnoa12345$"
+        while(line!=null){
             String[] split = line.split(","); // [0] "noa@gmail.com" [1] "Nnoa12345$"
-            list.add(new Object[]{User.builder().email(split[0]).password(split[1])});
-            line = bufferedReader.readLine();
+            list.add(new Object[]{User.builder().email(split[0]).password(split[1]).build()});
+            line =bufferedReader.readLine(); /// "sonya@gmail.com,Ss12345$"
         }
-        return list.iterator();
 
+        return list.iterator();
     }
+
+
 }
